55a744a637ef1faabfb107fe78a542a8140d231f
Arne Dussin <arne.dussin@live.de>
2018-02-24 16:11:35 +0100

    Start from scratch. Change Licence
    We decided to take the project into a different direction, which is why
    the stemmers will be taken out. The focus of the project will shift into
    a different, more modular direction.
    The licence change is rather minor and only because we wanted a licence
    catering to europe.

--- src/lib.rs
+++ src/lib.rs
@@ -1,33 +1,6 @@
-// unwort - A rule set-based language correction library.
-// Copyright (C) 2017 FOSS-AG TU Dortmund
+// Copyright (C) 2018 Arne Dußin.
 //
-// This library is free software; you can redistribute it and/or
-// modify it under the terms of the GNU Lesser General Public
-// License as published by the Free Software Foundation; either
-// version 2.1 of the License, or (at your option) any later version.
-// 
-// This library is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
-// 
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
-// USA
-
-//! The unwort library aims to be an a little cleverer dictionary, that tries
-//! to take into account the simplerer particularities of certain languages and
-//! avoid being a nuisance rather than a bane that you have to fight every step.
-//! Realising that it will most certainly never be possible to write the
-//! perfect spellchecker, it is a benevolent spellchecker, which should prefer
-//! not to mark an error that is one rather than marking an error that is not.
-
-#![warn(missing_docs)]
-
-extern crate rust_stemmers;
-
-pub mod dictionary;
-pub mod german;
-
-pub use dictionary::*;
+// This Program may be used by anyone in accordance with the terms of the
+// German Free Software License
+//
+// The License may be obtained under http://www.d-fsl.org.
